7ab39e7229e4dc19268d565075cbaf7dd1c5b6c7
GABRIEL FREIRE DE BARROS VIEIRA TADS031 <gabriel.vieira@edu.pe.senac.br>
2025-05-21 15:26:20 -0300

    changing main and deleting archives :fire:

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from dotenv import load_dotenv
-import os
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
-load_dotenv()
+app = FastAPI()
 
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str = None
+
+items_db: List[Item] = []
+
+
+@app.get("/items", response_model=List[Item])
+def get_items():
+    return items_db
+
+@app.post("/items", response_model=Item)
+def create_item(item: Item):
+    items_db.append(item)
+    return item
